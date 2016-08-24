f0d360e5ea5faa7eab6184da93678d5f36c1112f
ErikBoesen <me@erikboesen.com>
2016-08-23 21:36:06 -0400

    Auto-saving && git push

--- js/options.js
+++ js/options.js
@@ -1,27 +1,18 @@
-// Saves options to chrome.storage
-function save_options() {
-  var isClockMode = document.getElementById('clock').checked;
-  chrome.storage.sync.set({
-    isClockMode: isClockMode
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 1000);
-  });
-}
-
 // Restores select box and checkbox state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-  // Use default value clockMode = 'military' and isClockMode = false.
-  chrome.storage.sync.get({
-    isClockMode: false
-  }, function(items) {
-    document.getElementById('clock').checked = items.isClockMode;
-  });
+	// Use default value clockMode = 'military' and isClockMode = false.
+	chrome.storage.sync.get({
+		isClockMode: false
+	}, function(items) {
+		document.getElementById('clock').checked = items.isClockMode;
+	});
 }
 document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click', save_options);
+
+onchange = function() {
+    var isClockMode = document.getElementById('clock').checked;
+    chrome.storage.sync.set({
+        isClockMode: isClockMode
+    });
+};
